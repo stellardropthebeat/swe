0648f1606f8c8d4e908cacb077598d4f7454d2f9
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2023-01-28 17:54:42 +0700

    first pre-commit commit

--- homework1/backend/database/stock.py
+++ homework1/backend/database/stock.py
@@ -1,24 +1,33 @@
+"""Stock model."""
+
 from app_init import db
 
 
 class Stock(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vm_id = db.Column(db.Integer, db.ForeignKey('vending_machine.id'), nullable=False)
-    product = db.Column(db.String(255), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
+    """Stock model."""
 
-    def __init__(self, vm_id, product, quantity):
-        self.vm_id = vm_id
-        self.product = product
-        self.quantity = quantity
+    id: int = db.Column(db.Integer, primary_key=True)
+    vm_id: int = db.Column(
+        db.Integer, db.ForeignKey("vending_machine.id"), nullable=False
+    )
+    product: str = db.Column(db.String(255), nullable=False)
+    quantity: str = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
-        return '<Stock %r>' % self.product
+    def __init__(self: "Stock", stock: str, vm_id: int, quantity: str) -> None:
+        """Initialize a stock."""
+        self.vm_id: int = vm_id
+        self.product: str = stock
+        self.quantity: str = quantity
 
-    def to_dict(self):
+    def __repr__(self: "Stock") -> str:
+        """Represent a stock as a unique string."""
+        return "<Stock %r>" % self.product
+
+    def to_dict(self: "Stock") -> dict:
+        """Return object data in easily serializable format."""
         return {
-            'id': self.id,
-            'vm_id': self.vm_id,
-            'product': self.product,
-            'quantity': self.quantity
+            "id": self.id,
+            "vm_id": self.vm_id,
+            "product": self.product,
+            "quantity": self.quantity,
         }
