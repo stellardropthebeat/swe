c5e44429124d5544ef2f612754217591518a4bc3
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2023-01-28 18:07:56 +0700

    refactor process

--- homework1/backend/database/vending_machine.py
+++ homework1/backend/database/vending_machine.py
@@ -1,21 +1,24 @@
+"""Vending Machine Model."""
+
 from app_init import db
 
 
 class VendingMachine(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    location = db.Column(db.String(255), nullable=False)
+    """Vending Machine Model."""
 
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(255), nullable=False)
+    location: str = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return '<VendingMachine %r>' % self.name
+    def __init__(self: "VendingMachine", name: str, location: str) -> None:
+        """Initialize with name and location."""
+        self.name: str = name
+        self.location: str = location
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'location': self.location
-        }
+    def __repr__(self: "VendingMachine") -> str:
+        """Represent instance as a unique string."""
+        return "<VendingMachine %r>" % self.name
+
+    def to_dict(self: "VendingMachine") -> dict:
+        """Return object data in easily serializable format."""
+        return {"id": self.id, "name": self.name, "location": self.location}
