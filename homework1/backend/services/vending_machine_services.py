e702fc8cd04f1ab920afea2c70c3af273fd9f3bd
stellardropthebeat <nutchapol.isa@student.mahidol.edu>
2023-01-30 14:26:20 +0700

    refactor services

--- homework1/backend/services/vending_machine_services.py
+++ homework1/backend/services/vending_machine_services.py
@@ -1,29 +1,36 @@
-from database.stock import Stock
-from database.vending_machine import VendingMachine
+"""Vending Machine Services Module."""
+
 from app_init import db
+from database.vending_machine import VendingMachine
 
 
 class VendingMachineManager:
-    def __init__(self):
+    """Vending Machine Manager Class."""
+
+    def __init__(self: "VendingMachineManager") -> None:
+        """Initialize a vending machine manager."""
         self.db = db
 
-    def create_machine(self, name, location):
-        new_machine = VendingMachine(name=name, location=location)
+    def create_machine(self: "VendingMachineManager", name: str, location: str) -> None:
+        """Create a new vending machine in the vending machine table."""
+        new_machine: VendingMachine = VendingMachine(name=name, location=location)
         self.db.session.add(new_machine)
         self.db.session.commit()
         self.db.session.close()
 
-    def read_machine(self, id=None, name=None):
-        if id:
-            machine = VendingMachine.query.filter_by(id=id).first()
+    def read_machine(self: "VendingMachineManager", machine_id: int = None, name: str = None) -> VendingMachine:
+        """Read a vending machine from the vending machine table."""
+        if machine_id:
+            machine: VendingMachine = VendingMachine.query.filter_by(id=machine_id).first()
         elif name:
-            machine = VendingMachine.query.filter_by(name=name).first()
+            machine: VendingMachine = VendingMachine.query.filter_by(name=name).first()
         else:
-            machine = VendingMachine.query.all()
+            machine: VendingMachine = VendingMachine.query.all()
         return machine
 
-    def update_machine(self, id, name=None, location=None):
-        machine = VendingMachine.query.filter_by(id=id).first()
+    def update_machine(self: "VendingMachineManager", machine_id: int, name: str = None, location: str = None) -> None:
+        """Update a vending machine in the vending machine table."""
+        machine: VendingMachine = VendingMachine.query.filter_by(id=machine_id).first()
         if name:
             machine.name = name
         if location:
@@ -31,38 +38,9 @@ class VendingMachineManager:
         self.db.session.commit()
         self.db.session.close()
 
-    def delete_machine(self, id):
-        machine = VendingMachine.query.filter_by(id=id).first()
+    def delete_machine(self: "VendingMachineManager", machine_id: int) -> None:
+        """Delete a vending machine from the vending machine table."""
+        machine: VendingMachine = VendingMachine.query.filter_by(id=machine_id).first()
         self.db.session.delete(machine)
-        self.db.session.commit()
-        self.db.session.close()
-
-    def create_product(self, vm_id, product, quantity):
-        new_product = Stock(vm_id=vm_id, product=product, quantity=quantity)
-        self.db.session.add(new_product)
-        self.db.session.commit()
-        self.db.session.close()
-
-    def read_product(self, id=None, product=None):
-        if id:
-            product = Stock.query.filter_by(id=id).first()
-        elif product:
-            product = Stock.query.filter_by(product=product).first()
-        else:
-            product = Stock.query.all()
-        return product
-
-    def update_product(self, id, product=None, quantity=None):
-        stock = Stock.query.filter_by(id=id).first()
-        if product:
-            stock.product = product
-        if quantity:
-            stock.quantity = quantity
-        self.db.session.commit()
-        self.db.session.close()
-
-    def delete_product(self, id):
-        stock = Stock.query.filter_by(id=id).first()
-        self.db.session.delete(stock)
         self.db.session.commit()
         self.db.session.close()
