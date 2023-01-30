013eb5aca12b21648175b1a806571a40525e330e
stellardropthebeat <nutchapol.isa@student.mahidol.edu>
2023-01-30 14:33:52 +0700

    refactor app_init.py

--- homework1/backend/app_init.py
+++ homework1/backend/app_init.py
@@ -1,13 +1,16 @@
+"""Initialize the sqlalchemy database."""
+
+import os
+
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, database_exists
-import os
 
-url = os.environ.get('DATABASE_URL')
+url: os = os.environ.get("DATABASE_URL")
 
-db_engine = sqlalchemy.create_engine(url)
+db_engine: sqlalchemy = sqlalchemy.create_engine(url)
 
 if not database_exists(db_engine.url):
     create_database(db_engine.url)
 
-db = SQLAlchemy()
+db: sqlalchemy = SQLAlchemy()
