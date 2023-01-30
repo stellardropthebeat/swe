96112740d6b3fee87e5fddd09385aa01b9b24c88
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2023-01-30 12:22:10 +0700

    refactor stock_route.py

--- homework1/backend/route/stock_route.py
+++ homework1/backend/route/stock_route.py
@@ -1,61 +1,72 @@
-from flask import Blueprint, request, jsonify, render_template
-from services.vending_machine_services import VendingMachineManager
+"""Stock API route."""
+
 from database.stock import Stock
+from flask import Blueprint, Response, jsonify, render_template, request
+from services.vending_machine_services import VendingMachineManager
+from sqlalchemy.orm import collections
 
-stock_controller = Blueprint('stock_controller', __name__)
+stock_controller: Blueprint = Blueprint("stock_controller", __name__)
+
+failed_message: str = "Product not found"
 
 
-@stock_controller.route('/stock')
-def stocks():
-    return render_template('stock.html')
+@stock_controller.route("/stock")
+def stocks() -> str:
+    """Stock page."""
+    return render_template("stock.html")
 
 
-@stock_controller.route('/add_product', methods=['POST'])
-def add_product():
-    vm_id = request.json['vm_id']
-    product = request.json['product']
-    quantity = request.json['quantity']
-    manager = VendingMachineManager()
+@stock_controller.route("/add_product", methods=["POST"])
+def add_product() -> Response | tuple[Response, int]:
+    """Add product to vending machine."""
+    vm_id: int = request.json["vm_id"]
+    product: str = request.json["product"]
+    quantity: str = request.json["quantity"]
+    manager: VendingMachineManager = VendingMachineManager()
     manager.create_product(vm_id, product, quantity)
     return jsonify(success=True, message="Product added successfully"), 201
 
 
-@stock_controller.route('/update_product/<int:id>', methods=['PUT'])
-def update_product(id):
-    manager = VendingMachineManager()
-    product_to_update = manager.read_product(id=id)
-    if product_to_update:
-        product = request.json.get('product', product_to_update.product)
-        quantity = request.json.get('quantity', product_to_update.quantity)
-        manager.update_product(id, product=product, quantity=quantity)
+@stock_controller.route("/update_product/<int:id>", methods=["PUT"])
+def update_product(product_id: int) -> Response | tuple[Response, int]:
+    """Update product in vending machine."""
+    manager: VendingMachineManager = VendingMachineManager()
+    updated_product: Stock = manager.read_product(id=product_id)
+    if updated_product:
+        product: str = request.json.get("product", updated_product.product)
+        quantity: int = request.json.get("quantity", updated_product.quantity)
+        manager.update_product(product_id, product=product, quantity=quantity)
         return jsonify(success=True, message="product updated successfully")
     else:
         return jsonify(success=False, message="Vending machine not found"), 404
 
 
-@stock_controller.route('/get_product/<int:id>', methods=['GET'])
-def get_product(id):
-    manager = VendingMachineManager()
-    product = manager.read_product(id=id)
+@stock_controller.route("/get_product/<int:id>", methods=["GET"])
+def get_product(product_id: int) -> Response | tuple[Response, int]:
+    """Get product from vending machine."""
+    manager: VendingMachineManager = VendingMachineManager()
+    product: Stock = manager.read_product(id=product_id)
     if product:
         return jsonify(success=True, product=product.to_dict())
     else:
-        return jsonify(success=False, message="Product not found"), 404
+        return jsonify(success=False, message=failed_message), 404
 
 
-@stock_controller.route('/delete_product/<int:id>', methods=['DELETE'])
-def delete_product(id):
-    manager = VendingMachineManager()
-    product_to_delete = manager.read_product(id=id)
+@stock_controller.route("/delete_product/<int:id>", methods=["DELETE"])
+def delete_product(product_id: int) -> Response | tuple[Response, int]:
+    """Delete product from vending machine."""
+    manager: VendingMachineManager = VendingMachineManager()
+    product_to_delete: Stock = manager.read_product(id=product_id)
     if product_to_delete:
-        manager.delete_product(id)
-        return jsonify(success=True, message="Produt deleted successfully")
+        manager.delete_product(product_id)
+        return jsonify(success=True, message="Product deleted successfully")
     else:
-        return jsonify(success=False, message="Product not found"), 404
+        return jsonify(success=False, message=failed_message), 404
 
 
-@stock_controller.route('/all_products', methods=['GET'])
-def get_all_products():
-    products = Stock.query.all()
-    products_list = [product.to_dict() for product in products]
+@stock_controller.route("/all_products", methods=["GET"])
+def get_all_products() -> Response:
+    """Get all products from vending machine."""
+    products: collections.Iterable = Stock.query.all()
+    products_list: list = [product.to_dict() for product in products]
     return jsonify(products_list)
